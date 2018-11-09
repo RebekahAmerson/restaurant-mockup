37303e6160d4c73eb57a183b123f7e8ea4f306a0
Rebekah Amerson <becka_811@hotmail.com>
2018-11-09 11:04:49 -0600

    Component skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from'./Home.js';
+import About from'./About.js';
+import Menu from'./Menu.js';
+import Contact from'./Contact.js';
 
 class App extends Component {
+  state = {
+    page: 'home'
+  }
+
   render() {
+    const page = this.state.page;
+    console.log(page);
     return (
       <div id="main-page">
         <nav id="navbar">
           <h1 id="logo">5th Street Deli</h1>
           <ul>
-            <li className="nav-link">Home</li>
-            <li className="nav-link">About</li>
-            <li className="nav-link">Menu</li>
-            <li className="nav-link">Contact</li>
+            <li className="nav-link" onClick={() => this.setState({page: 'home'})}>Home</li>
+            <li className="nav-link" onClick={() => this.setState({page: 'about'})} >About</li>
+            <li className="nav-link" onClick={() => this.setState({page: 'menu'})}>Menu</li>
+            <li className="nav-link" onClick={() => this.setState({page: 'contact'})}>Contact</li>
           </ul>
         </nav>
+        <div>
+          {(() => {
+            switch(page) {
+              case 'home':
+                return <Home />;
+              case 'about':
+                return <About />;
+              case 'menu':
+                return <Menu />;
+              case 'contact':
+                return <Contact />;
+              default:
+              console.log(page);
+               return null;
+            }
+          })()}
+        </div>
       </div>
     );
   }
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return(
+      <div id="home-page">
+        <h2>Fresh Deals Every Day</h2>
+        <div id="home-pic">
+          <p>Photo by Jimi Filipovski on Unsplash</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
