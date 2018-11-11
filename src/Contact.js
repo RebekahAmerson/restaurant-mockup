da37354bd074cb5ff3a289c4e1914ab080f28c5a
Rebekah Amerson <becka_811@hotmail.com>
2018-11-11 16:43:03 -0600

    Contact component complete

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,32 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return(
-      <div className="tester">Contact</div>
+      <div id="contact">
+        <img src="images/lexie-barnhorn-583894-unsplash.jpg" alt="restaurant"/>
+        <div id="rest-info">
+          <div id="address">
+            <p>826 Sunshine Dr</p>
+            <p>Pittsburg, Ks 66762</p>
+            <p>620-555-5492</p>
+          </div>
+          <div id="hours">
+            <p className="rest-days">Monday</p>
+            <p className="rest-days">Tuesday</p>
+            <p className="rest-days">Wednesday</p>
+            <p className="rest-days">Thursday</p>
+            <p className="rest-days">Friday</p>
+            <p className="rest-days">Saturday</p>
+            <p className="rest-days">Sunday</p>
+            <p className="rest-hours">10:00am - 9:00pm</p>
+            <p className="rest-hours">10:00am - 9:00pm</p>
+            <p className="rest-hours">10:00am - 9:00pm</p>
+            <p className="rest-hours">10:00am - 9:00pm</p>
+            <p className="rest-hours">10:00am - 9:00pm</p>
+            <p className="rest-hours">11:00am - 5:00pm</p>
+            <p className="rest-hours">CLOSED</p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
