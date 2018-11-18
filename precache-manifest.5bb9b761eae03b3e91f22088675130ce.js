1a9dc1682d516dbe7f415a9862f7e187f44f7555
Rebekah Amerson <becka_811@hotmail.com>
2018-11-18 10:52:08 -0600

    Updates

---
+++ precache-manifest.5bb9b761eae03b3e91f22088675130ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "189e49463d06b94da189af2009fad802",
+    "url": "/RebekahAmerson/restaurant-mockup/static/media/jimi-filipovski-189721-unsplash.189e4946.jpg"
+  },
+  {
+    "revision": "890384e43e6c314353949e09e84ab0e4",
+    "url": "/RebekahAmerson/restaurant-mockup/static/media/edited-front.890384e4.png"
+  },
+  {
+    "revision": "4158e2ce508316017cf9",
+    "url": "/RebekahAmerson/restaurant-mockup/static/js/runtime~main.4158e2ce.js"
+  },
+  {
+    "revision": "d9de7e256a45a612b022",
+    "url": "/RebekahAmerson/restaurant-mockup/static/js/main.d9de7e25.chunk.js"
+  },
+  {
+    "revision": "e898c1d807665c468d22",
+    "url": "/RebekahAmerson/restaurant-mockup/static/js/1.e898c1d8.chunk.js"
+  },
+  {
+    "revision": "d9de7e256a45a612b022",
+    "url": "/RebekahAmerson/restaurant-mockup/static/css/main.4de48d02.chunk.css"
+  },
+  {
+    "revision": "3c3f443bdbd58d31aaaf2693b398ac9a",
+    "url": "/RebekahAmerson/restaurant-mockup/index.html"
+  }
+];
